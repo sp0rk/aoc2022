453d1e67d20823367757273fa86a2f09e7ae13f4
Julian Jurec <julian.jurec@gmail.com>
2022-12-01 19:52:18 +0100

    aoc1a: Add templates and input

--- src/main/kotlin/Aoc1a.kt
+++ src/main/kotlin/Aoc1a.kt
@@ -3,7 +3,7 @@ package com.github.spork.aoc2022
 import com.github.spork.aoc2022.core.Aoc
 
 object Aoc1a: Aoc {
-    override val inputPath = "inputs/Aoc1a.txt"
+    override val inputPath = "/inputs/Aoc1a.txt"
 
     override fun calculateAnswer(input: String) : String {
         return input
